a0c7ad80f1bf3494cbcba0d4e6e9bba125547cf1
JAISHRIKRISHNA\Manish <manish.awsprep@gmail.com>
2020-07-29 13:13:23 +0530

    Adding App.Java from testrepo/src/main/java/com/technochanakya/testrepo

---
+++ testrepo/src/main/java/com/technochanakya/testrepo/App.java
@@ -0,0 +1,13 @@
+package com.technochanakya.testrepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
